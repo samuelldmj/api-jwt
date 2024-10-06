861c9ed8d25435b90ddd193aa44a8b63ce470d13
Samuel <samuelldmj5@gmail.com>
2024-10-06 16:48:50 +0100

    create file modified

--- classes/Users.php
+++ classes/Users.php
@@ -20,6 +20,46 @@ class Users
     $this->project_tbl = 'tbl_project';
   }
 
+  public function create_user(){
+    $sql_query = "INSERT INTO {$this->users_tbl} (user_name, user_email, user_password) VALUES ( ?,?,?)";
+    
+    $stmt = $this->conn->prepare($sql_query);
+
+    //sanitize
+    $this->user_name = htmlspecialchars(strip_tags($this->user_name));
+    $this->user_email = htmlspecialchars(strip_tags($this->user_email));
+    $this->user_password =  htmlspecialchars(strip_tags($this->user_password));
+
+         // Bind parameters
+         $stmt->bindValue(1, $this->user_name);
+         $stmt->bindValue(2, $this->user_email);
+         $stmt->bindValue(3, $this->user_password);
+ 
+         if ($stmt->execute()) {
+             return true;
+         }
+         return false;
+     }
+ 
+     public function check_email()
+     {
+         $sql_query = "SELECT * FROM " . $this->users_tbl . "WHERE email = ? ";
+ 
+         $stmt = $this->conn->prepare($sql_query);
+ 
+         $stmt->bindValue(1, $this->email);
+ 
+         if ($stmt->execute()) {
+             $data = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+             return $data;
+         }
+ 
+         return [];
+     }
+
+
+  }
 
 
 
@@ -28,7 +68,3 @@ class Users
 
 
 
-
-
-
-}
--- v1/create.php
+++ v1/create.php
@@ -1,3 +1,58 @@
 <?php
 
-echo "Hello world";
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Method: POST");
+header("Content-type: application/json; charset=UTF-8");
+
+//INCLUDING FILES
+include_once "../config/Database.php";
+include_once "../classes/Users.php";
+
+// Create a database object and establish connection
+$db = new Database();
+$connection = $db->connect(); // Get the PDO connection
+
+// Pass the PDO connection to the Users object
+$user_obj = new Users($connection);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    /*
+    Retrieves the raw data sent in the request body
+    (typically used in POST requests when the content is in JSON format).
+    */
+    $data = json_decode(file_get_contents("php://input"));
+
+    if (!empty($data->user_name) && !empty($data->user_email) && !empty($data->user_password)) {
+
+        $user_obj->user_name = $data->user_name;
+        $user_obj->user_email = $data->user_email;
+        $user_obj->user_password = password_hash($data->user_password, PASSWORD_DEFAULT);
+
+        if ($user_obj->create_user()) {
+            http_response_code(200);
+            echo json_encode([
+                'status' => 1,
+                'message' => "User has been created"
+            ]);
+        } else {
+            http_response_code(500);
+            echo json_encode([
+                'status' => 0,
+                'message' => 'Failed to Save user data'
+            ]);
+        }
+    } else {
+        http_response_code(500);
+        echo json_encode([
+            'status' => 0,
+            'message' => 'All data required'
+        ]);
+    }
+} else {
+    http_response_code(503);
+    echo json_encode([
+        'status' => 0,
+        'message' => 'Access Denied!'
+    ]);
+}
